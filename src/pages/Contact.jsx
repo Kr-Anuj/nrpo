d9ec521eb39f973c05ac1db5e1a3c230a30fb1f0
Kr-Anuj <155348877+Kr-Anuj@users.noreply.github.com>
2024-07-14 18:15:50 +0530

    Update Contact.jsx
    final commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,11 +33,6 @@ const Contact = () => {
     message: form.message,
   };
 
-  // Log environment variables
-  console.log('EmailJS Service ID:', import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
-  console.log('EmailJS Template ID:', import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
-  console.log('EmailJS Public Key:', import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
-
   emailjs.send(
     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
